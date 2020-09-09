4ae7adbacf1276f75b540200077593f3c8bc8250
JY8752 <j.y.8752@gmail.com>
2020-09-09 19:54:22 +0900

    first commit

---
+++ src/main/java/com/example/riot2/Riot2Application.java
@@ -0,0 +1,13 @@
+package com.example.riot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Riot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Riot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/riot2/Riot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Riot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
