60942657a4670938014a983b26444a5ca476249e
JY8752 <j.y.8752@gmail.com>
2020-09-22 21:05:17 +0900

    もろもろ

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,10 +1,9 @@
 import '@riotjs/hot-reload'
-import { component } from 'riot'
+import { component, register } from 'riot'
+import { Router, Route } from '@riotjs/route'
 import 'semantic-ui-riot'
 import App from './app.riot'
 
-// register('router', Router)
-// register('route', Route)
-component(App)(document.getElementById('app'), {
-    title: 'Riot Route Demo'
-})
+register('router', Router)
+register('route', Route)
+component(App)(document.getElementById('app'))
